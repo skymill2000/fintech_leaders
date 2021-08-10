f7f8d257ea8dd1fc6d2009edb14748c40c7f258e
skymill2000 <skymill2000@gmail.com>
2021-08-10 19:48:09 +0900

    최초의 커밋입니다.

---
+++ myfintechproject/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          안녕하세요 <code>핀테크리더스과정 참여자 여러분</code> 화이팅.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
