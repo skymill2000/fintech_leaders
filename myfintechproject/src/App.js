f2c3db68bbf06ef2f608424b5a227f4898378289
skymill2000 <skymill2000@gmail.com>
2021-08-10 21:20:26 +0900

    컴포넌트와 프롭스 !

--- myfintechproject/src/App.js
+++ myfintechproject/src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
-const Header = () => {
-  return(<>
-    <h1>헤더입니다.</h1>
+const Header = ({title}) => {
+  return(
+  <>
+    <h1>{title} 입니다.</h1>
   </>)
 }
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          안녕하세요 <code>핀테크리더스과정 참여자 여러분</code> 화이팅.
-        </p>
-      </header>
-    </div>
+    <>
+      <Header title="첫번째"></Header>
+      <Header></Header>
+      <Header></Header>
+      <Header></Header>
+    </>
   );
 }
 
