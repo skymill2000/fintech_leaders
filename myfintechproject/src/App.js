f8c1554775203f408c2cbf0175ea076f13ee35b7
skymill2000 <skymill2000@gmail.com>
2021-08-10 21:33:17 +0900

    입력 이벤트 처리

--- myfintechproject/src/App.js
+++ myfintechproject/src/App.js
@@ -14,12 +14,17 @@ function App() {
     alert("버튼 클릭하셨습니다.")
   }
 
+  const handleChange = () => {
+    console.log("데이터가 변경되었습니다.")//? 입력 데이터는?    
+  }
+
   return (
     <>
       <Header title="첫번째"></Header>
       <Header ></Header>
       <Header></Header>
       <Header></Header>
+      <input onChange={handleChange}></input>
       <button onClick={clickEvent}>이벤트 버튼입니다.</button>
     </>
   );
