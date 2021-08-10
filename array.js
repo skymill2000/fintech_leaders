1ac42752b619c3204f95230ce25f0b84c2a5c512
skymill2000 <skymill2000@gmail.com>
2021-08-10 20:41:47 +0900

    반복문

---
+++ array.js
@@ -0,0 +1,12 @@
+let car = "car"
+let cars = ["car1", "car2", "car3", car];
+//public String [] array = new String[3];
+
+console.log(cars[0])
+console.log(cars[1])
+console.log(cars[2])
+console.log(cars[3])
+
+cars.map((car)=>{
+    console.log(car);
+})
