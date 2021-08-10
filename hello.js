b5882bbe012cadcec5922cd946dc938b77959b1d
skymill2000 <skymill2000@gmail.com>
2021-08-10 20:14:42 +0900

    변수 / 오브젝트 간단 실습

--- hello.js
+++ hello.js
@@ -1,1 +1,18 @@
-console.log("hello");
+// console.log("hello");
+//
+let data = "hello let";
+const dataConst = "hello const";
+
+// console.log(data);
+// console.log(dataConst);
+
+let car = {
+    name : "sonata",
+    ph : 160,
+    start : () => {
+        console.log("engine Start");
+    }
+}
+
+console.log(car.name);
+car.start();
