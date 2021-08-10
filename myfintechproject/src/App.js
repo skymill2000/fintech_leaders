69a594f0cc8a990bf80e51adf2862c2ef4c01b19
skymill2000 <skymill2000@gmail.com>
2021-08-10 21:15:34 +0900

    헤더 컴포넌트 추가

--- myfintechproject/src/App.js
+++ myfintechproject/src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Header = () => {
+  return(<>
+    <h1>헤더입니다.</h1>
+  </>)
+}
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           안녕하세요 <code>핀테크리더스과정 참여자 여러분</code> 화이팅.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
