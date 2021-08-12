6573faba270d2764d785b649ec0d271a00710ac7
skymill2000 <skymill2000@gmail.com>
2021-08-12 19:09:51 +0900

    버그수정

--- myfintechproject/src/App.js
+++ myfintechproject/src/App.js
@@ -1,20 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import AxiosComponent from "./component/AxiosComponent";
-import ListComponent from "./component/ListComponent";
-import AuthResultPage from "./pages/AuthResultPage";
-import BalancePage from "./pages/BalancePage";
-import ListPage from "./pages/ListPage";
-import MainPage from "./pages/MainPage";
-import NewsPage from "./pages/NewsPage";
-import QrCodePage from "./pages/QrCodePage";
-import QrReader from "./pages/QrReader";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/axios" exact>
+        <Route path="/test" exact>
         </Route>
       </Switch>
     </Router>
