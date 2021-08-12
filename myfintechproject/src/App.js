0f137d1db5610efcf37e827e7ec5b69e22be1085
skymill2000 <skymill2000@gmail.com>
2021-08-12 19:20:18 +0900

    라우터 연결 / 페이지 추가

--- myfintechproject/src/App.js
+++ myfintechproject/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AxiosTest from "./pages/AxiosTest";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/test" exact>
-          테스트
+          <AxiosTest></AxiosTest>
         </Route>
       </Switch>
     </Router>
--- myfintechproject/src/pages/AxiosTest.js
+++ myfintechproject/src/pages/AxiosTest.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const AxiosTest = () => {
+    const handleClick = () =>{
+        console.log("이벤트 발생");
+    }
     return (
         <div>
-            
+            <button onClick={handleClick}>데이터 가져오기!</button>
         </div>
     )
 }
